d6e0544768bb1479d0287ce89606cec1f5f7c8f4
vmcl <none@none>
2014-04-18 21:51:16 -0400

    ChessPlatform.UI.Desktop: Catching unhandled exceptions

--- src/ChessPlatform.UI.Desktop/App.xaml.cs
+++ src/ChessPlatform.UI.Desktop/App.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 
@@ -10,7 +9,30 @@ namespace ChessPlatform.UI.Desktop
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        #region Protected Methods
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+        }
+
+        private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs args)
+        {
+            var window = Current.Morph(obj => obj.MainWindow);
+
+            MessageBox.Show(
+                window,
+                args.ExceptionObject.ToStringSafely("<Unknown exception>"),
+                "Unhandled error",
+                MessageBoxButton.OK);
+
+            Process.GetCurrentProcess().Kill();
+        }
+
+        #endregion
     }
 }
