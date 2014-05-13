af05c4a10010bb760331117dc02335392ab54e7f
vmcl <none@none>
2014-05-12 21:03:46 -0400

    ChessPlatform.UI.Desktop: Minor refactoring (part 2)

--- src/ChessPlatform.UI.Desktop/Commands/GameWindowCommands.cs
+++ src/ChessPlatform.UI.Desktop/Commands/GameWindowCommands.cs
@@ -4,38 +4,38 @@ using System.Windows.Input;
 
 namespace ChessPlatform.UI.Desktop.Commands
 {
-    internal static class MainWindowCommands
+    internal static class GameWindowCommands
     {
         #region Constants and Fields
 
         public static readonly RoutedUICommand NewGame = new RoutedUICommand(
             "_New Game...",
             "NewGame",
-            typeof(MainWindowCommands),
+            typeof(GameWindowCommands),
             new InputGestureCollection { new KeyGesture(Key.N, ModifierKeys.Control) });
 
         public static readonly RoutedUICommand NewGameFromFenFromClipboard = new RoutedUICommand(
             "New Game from _FEN from Clipboard...",
             "NewGameFromFenFromClipboard",
-            typeof(MainWindowCommands),
+            typeof(GameWindowCommands),
             new InputGestureCollection { new KeyGesture(Key.V, ModifierKeys.Control | ModifierKeys.Shift) });
 
         public static readonly RoutedUICommand Exit = new RoutedUICommand(
             "E_xit",
             "Exit",
-            typeof(MainWindowCommands),
+            typeof(GameWindowCommands),
             new InputGestureCollection { new KeyGesture(Key.Q, ModifierKeys.Control) });
 
         public static readonly RoutedUICommand UndoLastMove = new RoutedUICommand(
             "_Undo Last Move...",
             "UndoLastMove",
-            typeof(MainWindowCommands),
+            typeof(GameWindowCommands),
             new InputGestureCollection { new KeyGesture(Key.Z, ModifierKeys.Control) });
 
         public static readonly RoutedUICommand CopyFenToClipboard = new RoutedUICommand(
             "_Copy FEN to Clipboard",
             "CopyFenToClipboard",
-            typeof(MainWindowCommands),
+            typeof(GameWindowCommands),
             new InputGestureCollection { new KeyGesture(Key.C, ModifierKeys.Control) });
 
         #endregion
