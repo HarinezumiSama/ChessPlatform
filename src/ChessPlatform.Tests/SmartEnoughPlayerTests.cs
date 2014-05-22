12d83ceef357ff87328ebe5dd8191d49ed2f4d6c
vmcl <none@none>
2014-05-22 02:32:03 -0400

    SmartEnoughPlayerTests has been introduced

---
+++ src/ChessPlatform.Tests/SmartEnoughPlayerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using ChessPlatform.ComputerPlayers;
+using NUnit.Framework;
+
+namespace ChessPlatform.Tests
+{
+    [TestFixture]
+    public sealed class SmartEnoughPlayerTests
+    {
+        #region Tests
+
+        [Test]
+        public void TestPerformanceOfGetMoveForPosition1()
+        {
+            const string Fen = "r1bqkbnr/pppp1ppp/4p3/n7/4P3/3B1N2/PPPP1PPP/RNBQK2R b KQkq - 3 4";
+            var gameBoard = new GameBoard(Fen);
+
+            var player = new SmartEnoughPlayer(PieceColor.Black, 4);
+
+            var stopwatch = Stopwatch.StartNew();
+            var task = player.GetMove(gameBoard, CancellationToken.None);
+            task.Start();
+            var move = task.Result;
+            stopwatch.Stop();
+
+            Console.WriteLine(
+                "[{0}] GetMove took {1} (move {2}).",
+                MethodBase.GetCurrentMethod().GetQualifiedName(),
+                stopwatch.Elapsed,
+                move);
+
+            Assert.That(move, Is.Not.Null);
+        }
+
+        #endregion
+    }
+}
