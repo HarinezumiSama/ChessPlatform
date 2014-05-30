ad1b8ed848b3125f17e67c0f82e118cb82c44301
vmcl <none@none>
2014-05-30 02:17:51 -0400

    [1] ChessPlatform.UI.Desktop: Adjusted project settings [2] Implemented DLL and EXE versioning

--- src/ChessPlatform.ComputerPlayers/Properties/AssemblyInfo.cs
+++ src/ChessPlatform.ComputerPlayers/Properties/AssemblyInfo.cs
@@ -1,38 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
 [assembly: AssemblyTitle("ChessPlatform.ComputerPlayers")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ChessPlatform")]
-[assembly: AssemblyCopyright("Copyright © Vitalii Maklai 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("0cab642e-571f-49ba-8884-2b76d9e22794")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Solution Items/CommonAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Chess Platform: Simple .NET based chess engine and computer players.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Vitalii Maklai")]
+[assembly: AssemblyProduct("Chess Platform")]
+[assembly: AssemblyCopyright("Copyright © Vitalii Maklai 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
