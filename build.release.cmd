ff70ed08d3d98159fb9137a6fcfd471d71bcca82
vmcl <none@none>
2014-06-04 06:39:23 -0400

    Minor improvement of the build script

--- build.release.cmd
+++ build.release.cmd
@@ -2,7 +2,7 @@
 
 setlocal
 
-set MSBUILD=C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+set MSBUILD=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 set PROJECT=%~dp0\src\ChessPlatform.sln
 
 "%MSBUILD%" "%PROJECT%" /t:Rebuild /p:Configuration="Release" /p:Platform="Any CPU" || goto ERROR
