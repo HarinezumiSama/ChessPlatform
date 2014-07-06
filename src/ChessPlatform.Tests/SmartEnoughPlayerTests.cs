6c9c5aa2445218886c6cafbbc16c8a65092bee8b
vmcl <none@none>
2014-07-06 10:36:10 -0400

    SmartEnoughPlayerTests: Added the test for computer's blunder when it gives up a queen (depth 5 only)

--- src/ChessPlatform.Tests/SmartEnoughPlayerTests.cs
+++ src/ChessPlatform.Tests/SmartEnoughPlayerTests.cs
@@ -25,9 +25,35 @@ namespace ChessPlatform.Tests
         [TestCase(9, Explicit = true)]
         public void TestPerformanceOfGetMoveForPosition1(int maxPlyDepth)
         {
-            var gameBoard = new GameBoard("r1bqkbnr/pppp1ppp/4p3/n7/4P3/3B1N2/PPPP1PPP/RNBQK2R b KQkq - 3 4");
+            const string Fen = "r1bqkbnr/pppp1ppp/4p3/n7/4P3/3B1N2/PPPP1PPP/RNBQK2R b KQkq - 3 4";
+            ExecuteTestForFenAndDepth(Fen, maxPlyDepth);
+        }
 
-            var player = new SmartEnoughPlayer(PieceColor.Black, maxPlyDepth, false, null);
+        [Test]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6, Explicit = true)]
+        [TestCase(7, Explicit = true)]
+        [TestCase(8, Explicit = true)]
+        [TestCase(9, Explicit = true)]
+        public void TestPerformanceOfGetMoveForQueenUnderAttack(int maxPlyDepth)
+        {
+            // Currently, the issue occurs at depth 5, where the computer player gives up a queen by move a2-a3
+            const string Fen = "rn3rk1/ppp2ppb/3qp2p/8/1b2P3/2Q3P1/PPPN1PBP/R1B2RK1 w - - 5 13";
+            ExecuteTestForFenAndDepth(Fen, maxPlyDepth);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static void ExecuteTestForFenAndDepth(string fen, int maxPlyDepth)
+        {
+            var gameBoard = new GameBoard(fen);
+
+            var player = new SmartEnoughPlayer(gameBoard.ActiveColor, maxPlyDepth, false, null);
 
             var stopwatch = Stopwatch.StartNew();
             var task = player.CreateGetMoveTask(new GetMoveRequest(gameBoard, CancellationToken.None));
