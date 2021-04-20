643ee0a18acea0ee7dbb40ac6a02faa9ee16bd36
Vitalii Maklai <vitalii.maklai@gmail.com>
2021-04-20 16:19:21 -0400

    Temporary fix of the build process after migration from Mercurial to Git

---
+++ src/Solution Items/Version/Generated.GlobalVersion.cs
@@ -0,0 +1,6 @@
+﻿//// Temporary stub for post Mercurial-to-Git migration
+
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0-post.migration.to.git")]
